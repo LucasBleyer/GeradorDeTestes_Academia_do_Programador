eed72aad0a10386a928abfb00a649f043916e244
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-05-13 22:32:20 -0300

    Add project files.

---
+++ Testes.WinApp/ModuloQuestões/TelaCadastroQuestaoForm.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace Testes.WinApp.ModuloQuestões
+{
+    partial class TelaCadastroQuestaoForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.cbxDisciplina = new System.Windows.Forms.ComboBox();
+            this.cbxMateria = new System.Windows.Forms.ComboBox();
+            this.txtTitulo = new System.Windows.Forms.TextBox();
+            this.txtEnunciado = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtC = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtD = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.cbxGabarito = new System.Windows.Forms.ComboBox();
+            this.btnGravar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Disciplina:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(429, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Matérias:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Título:";
+            // 
+            // cbxDisciplina
+            // 
+            this.cbxDisciplina.FormattingEnabled = true;
+            this.cbxDisciplina.Location = new System.Drawing.Point(79, 86);
+            this.cbxDisciplina.Name = "cbxDisciplina";
+            this.cbxDisciplina.Size = new System.Drawing.Size(329, 23);
+            this.cbxDisciplina.TabIndex = 3;
+            // 
+            // cbxMateria
+            // 
+            this.cbxMateria.FormattingEnabled = true;
+            this.cbxMateria.Location = new System.Drawing.Point(490, 86);
+            this.cbxMateria.Name = "cbxMateria";
+            this.cbxMateria.Size = new System.Drawing.Size(211, 23);
+            this.cbxMateria.TabIndex = 4;
+            // 
+            // txtTitulo
+            // 
+            this.txtTitulo.Location = new System.Drawing.Point(79, 131);
+            this.txtTitulo.Name = "txtTitulo";
+            this.txtTitulo.Size = new System.Drawing.Size(329, 23);
+            this.txtTitulo.TabIndex = 5;
+            // 
+            // txtEnunciado
+            // 
+            this.txtEnunciado.Location = new System.Drawing.Point(25, 192);
+            this.txtEnunciado.Name = "txtEnunciado";
+            this.txtEnunciado.Size = new System.Drawing.Size(675, 146);
+            this.txtEnunciado.TabIndex = 6;
+            this.txtEnunciado.Text = "";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 174);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(129, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Enunciado da Questão:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(24, 357);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(130, 15);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Alternativas da questão";
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(39, 380);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(661, 23);
+            this.txtA.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(22, 380);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(17, 15);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "a)";
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(39, 409);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(661, 23);
+            this.txtB.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(22, 409);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(18, 15);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "b)";
+            // 
+            // txtC
+            // 
+            this.txtC.Location = new System.Drawing.Point(39, 438);
+            this.txtC.Name = "txtC";
+            this.txtC.Size = new System.Drawing.Size(661, 23);
+            this.txtC.TabIndex = 14;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(22, 438);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(17, 15);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "c)";
+            // 
+            // txtD
+            // 
+            this.txtD.Location = new System.Drawing.Point(39, 467);
+            this.txtD.Name = "txtD";
+            this.txtD.Size = new System.Drawing.Size(661, 23);
+            this.txtD.TabIndex = 16;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(22, 467);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(18, 15);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "d)";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(39, 509);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(52, 15);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "Gabarito";
+            // 
+            // cbxGabarito
+            // 
+            this.cbxGabarito.FormattingEnabled = true;
+            this.cbxGabarito.Location = new System.Drawing.Point(93, 506);
+            this.cbxGabarito.Name = "cbxGabarito";
+            this.cbxGabarito.Size = new System.Drawing.Size(121, 23);
+            this.cbxGabarito.TabIndex = 18;
+            // 
+            // btnGravar
+            // 
+            this.btnGravar.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnGravar.Location = new System.Drawing.Point(476, 518);
+            this.btnGravar.Name = "btnGravar";
+            this.btnGravar.Size = new System.Drawing.Size(104, 49);
+            this.btnGravar.TabIndex = 19;
+            this.btnGravar.Text = "Cadastrar";
+            this.btnGravar.UseVisualStyleBackColor = true;
+            this.btnGravar.Click += new System.EventHandler(this.btnGravar_Click_1);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancelar.Location = new System.Drawing.Point(596, 518);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(104, 49);
+            this.btnCancelar.TabIndex = 20;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // TelaCadastroQuestaoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(721, 597);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnGravar);
+            this.Controls.Add(this.cbxGabarito);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.txtD);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtC);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtEnunciado);
+            this.Controls.Add(this.txtTitulo);
+            this.Controls.Add(this.cbxMateria);
+            this.Controls.Add(this.cbxDisciplina);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "TelaCadastroQuestaoForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cadastro de Matérias";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.ComboBox cbxDisciplina;
+        private System.Windows.Forms.ComboBox cbxMateria;
+        private System.Windows.Forms.TextBox txtTitulo;
+        private System.Windows.Forms.RichTextBox txtEnunciado;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtC;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtD;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.ComboBox cbxGabarito;
+        private System.Windows.Forms.Button btnGravar;
+        private System.Windows.Forms.Button btnCancelar;
+    }
+}
